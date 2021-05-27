cf60a2de018f38192500ee5ee52cf3bd4146479a
akop <56491stR_>
2021-05-28 01:52:10 +0300

    a bit updated

--- tests/search_test.py
+++ tests/search_test.py
@@ -5,8 +5,7 @@ import pytest
 
 def test_search_for_products(environment_configuration):
         homepage = HomePage(environment_configuration)
-        search_results = homepage.search()
-        search_results.search_for('earphones')
+        search_results = homepage.search().search_for('earphones')
         assert 3 == search_results.product_count
         product = search_results.open_product_page('MADISON EARBUDS')
         assert product.name == 'MADISON EARBUDS'
