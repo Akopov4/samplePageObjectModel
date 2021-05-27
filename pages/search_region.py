be4fa995e8dbf1db120af6a16ef7d73ce330c6c3
akop <56491stR_>
2021-05-28 02:21:45 +0300

    fixed issue with search field locator

--- pages/search_region.py
+++ pages/search_region.py
@@ -1,6 +1,9 @@
 from base.base_page import BasePage
 from pages.search_results_page import SearchResults
 from locators.search_locators import SearchLocators as SL
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class SearchRegion(BasePage):
 
@@ -9,7 +12,8 @@ class SearchRegion(BasePage):
 
     def search_for(self,term):
 
-        self.search_field = self.driver.find_element_by_name(SL.search_box_locator)
+        # self.search_field = self.driver.find_element_by_name(SL.search_box_locator)
+        self.search_field = WebDriverWait(self.driver, 20).until(EC.presence_of_element_located((By.NAME,'q')))
         self.search_field.clear()
         self.search_field.send_keys(term)
         self.search_field.submit()
